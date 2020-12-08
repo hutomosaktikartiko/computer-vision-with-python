4e8562987e48b189037891bb2cf256a4a0f010d5
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2020-12-08 11:55:12 +0700

    Read Video

---
+++ Read_Video.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+cap = cv.VideoCapture("Videos/Rebellion Rose - Terima Kasih (Officia) Video Lirik.mp4")
+
+while(True):
+    ret, frame = cap.read()
+
+    cv.imshow("Frame", frame)
+
+    if cv.waitKey(1) & 0xFF == ord("e"):
+        break
+
+cap.release()
+cv.destroyAllWindows()
