34db94dcee65fc117964c10c145cb787cf417fa6
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2020-12-08 11:36:31 +0700

    Basic Operations in OpenCV

--- Basic_Operations_OpenCv.py
+++ Basic_Operations_OpenCv.py
@@ -1,3 +1,13 @@
-import cv2
+import cv2 as cv
 
+img = cv.imread("New-Ronaldo.jpg", 1)
 
+cv.imshow("Image", img)
+
+e = cv.waitKey()
+
+if e == 27:
+    cv.destroyAllWindows()
+elif e == ord("s"):
+    cv.imwrite("The-New-Ronaldo.jpg", img)
+    cv.destroyAllWindows()
