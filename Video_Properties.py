16ddb497703941564000a062b409c54ad2f1a418
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2020-12-09 11:32:50 +0700

    Video Properties OpenCV

---
+++ Video_Properties.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    print(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+    cv.imshow("Frame", frame)
+
+    if cv.waitKey(1) & 0xFF == ord("e"):
+        break
+
+cap.release()
+cv.destroyAllWindows()
