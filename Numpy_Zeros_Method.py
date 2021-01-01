4bc80276587de24f8a2c03f196f1af1c8cc2f58a
hutomosakti <hutomosaktikartiko.siskom@gmail.com>
2021-01-02 01:35:33 +0700

    Update Numpy Zeros Method

--- Numpy_Zeros_Method.py
+++ Numpy_Zeros_Method.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2 as cv
 
-img = np.zeros([300, 400, 3], np.uint8)
+img = np.zeros([512, 512, 3], np.uint8)
+
+img = cv.circle(img, (200, 200), 100, (0, 255, 255), 1)
 
 cv.imshow("Image", img)
 
